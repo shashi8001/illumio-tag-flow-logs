34b3d33c14c34c1869a9183b100ed0c20ab5fcc8
shashi9492 <shashidharthanga@gmail.com>
2024-08-23 11:12:27 -0700

    Refinements

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ package org.shashidharkumar.src;
 public class Main {
     public static void main(String[] args) {
 
-        //The file path have to be updated based on the input files location
+        //The file path have to be updated based on the input and output file's location
         String flowLogFile = "C:\\Users\\shash\\Documents\\Learning\\Spring\\Illumio-tag-network-logs\\src\\input\\flow-logs";
         String lookUpFile = "C:\\Users\\shash\\Documents\\Learning\\Spring\\Illumio-tag-network-logs\\src\\input\\look-up-table";
         String protocolFile = "C:\\Users\\shash\\Documents\\Learning\\Spring\\Illumio-tag-network-logs\\src\\input\\protocol-numbers-1.csv";
